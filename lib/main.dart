adaa97669139f380e3fd810a9bd6ee0d17f86f44
Viktor Popp <viktorpopp.dev@gmail.com>
2024-12-09 10:56:51 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:supabase_chat/cubits/profiles/profiles_cubit.dart';
+import 'package:supabase_chat/utils/constants.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_chat/pages/splash_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: urlkey,
+    anonKey: anonkey,
+    authCallbackUrlHostname: 'login',
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<ProfilesCubit>(
+      create: (context) => ProfilesCubit(),
+      child: MaterialApp(
+        title: 'Supabase Chat',
+        debugShowCheckedModeBanner: false,
+        theme: appTheme,
+        home: const SplashPage(),
+      ),
+    );
+  }
+}
